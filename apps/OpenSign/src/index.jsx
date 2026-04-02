c3031b2da0a9ac166875c023a362a28feec68dcb
No-Durian <940779680@qq.com>
2026-04-02 08:43:23 +0800

    chore: remove message board and disable parse login stack in enterprise mode

--- apps/OpenSign/src/index.jsx
+++ apps/OpenSign/src/index.jsx
@@ -5,16 +5,8 @@ import App from "./App";
 import { showUpgradeProgress, hideUpgradeProgress } from "./utils";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import Parse from "parse";
 import "./polyfills";
-import { serverUrl_fn } from "./constant/appinfo";
 import "./i18n";
-
-const appId =
-  import.meta.env.VITE_APPID || process.env.REACT_APP_APPID || "opensign";
-const serverUrl = serverUrl_fn();
-Parse.initialize(appId);
-Parse.serverURL = serverUrl;
 
 if (localStorage.getItem("showUpgradeProgress")) {
   showUpgradeProgress();
